be61b6b04f9606b0909f173fdb3902eee54083e8
Joshua Sims <jm8hjkl@gmail.com>
2025-06-20 19:36:26 -0400

    Organize

--- test/test.sh
+++ test/test.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
-for f in $(find test/*  -maxdepth 1 -type d); do
+export PATH="$PATH:$(dirname $(dirname $0))/src"
+for f in $(find $(dirname $0)/*  -maxdepth 1 -type d); do
     echo
     echo $f
 
-    ./cc.awk $f/test.c > $f/test.s || { echo compilation failed; continue; }
+    cc.awk $f/test.c > $f/test.s || { echo compilation failed; continue; }
     riscv64-unknown-linux-musl-gcc $f/harness.c $f/test.s -o $f/a.out || { echo assemblation failed; continue; }
     $f/a.out && echo success || echo failed
 done
