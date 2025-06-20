bbe48b8851f5d75893d3c37eb15118ebd0e5fb05
Joshua Sims <jm8hjkl@gmail.com>
2025-06-20 19:38:21 -0400

    Exit code

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
-export PATH="$PATH:$(dirname $(dirname $0))/src"
+PATH="$PATH:$(dirname $(dirname $0))/src"
+bad=0
 for f in $(find $(dirname $0)/*  -maxdepth 1 -type d); do
     echo
     echo $f
 
-    cc.awk $f/test.c > $f/test.s || { echo compilation failed; continue; }
-    riscv64-unknown-linux-musl-gcc $f/harness.c $f/test.s -o $f/a.out || { echo assemblation failed; continue; }
-    $f/a.out && echo success || echo failed
+    cc.awk $f/test.c > $f/test.s || { echo compilation failed; bad=1; continue; }
+    riscv64-unknown-linux-musl-gcc $f/harness.c $f/test.s -o $f/a.out || { echo assemblation failed; bad=1; continue; }
+    $f/a.out && echo success || { echo failed; bad=1; }
 done
+
+exit $bad
